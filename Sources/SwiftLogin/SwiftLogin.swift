9d0575009aa5d8e33fa36435acd5065fc53a2fb7
Sergio <bostshakur@gmail.com>
2022-06-07 18:47:16 -0700

    initial commit

--- Sources/SwiftLogin/SwiftLogin.swift
+++ Sources/SwiftLogin/SwiftLogin.swift
@@ -2,5 +2,10 @@ public struct SwiftLogin {
     public private(set) var text = "Hello, World!"
 
     public init() {
+
+    }
+    
+    public func printHello() {
+        print("Hello!")
     }
 }
